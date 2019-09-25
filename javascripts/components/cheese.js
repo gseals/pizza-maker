33dfb59823f233bb70f8aaa5dc1a5791e175deba
Gabriel Seals <sealsgj@gmail.com>
2019-09-24 19:56:04 -0500

    added radion buttons for cheese choices

--- javascripts/components/cheese.js
+++ javascripts/components/cheese.js
@@ -1,9 +1,21 @@
 import utilities from '../helpers/utilities.js';
 
+const cheeses = [
+    {id: "cheese1", name: "swiss", price: 50},
+    {id: "cheese2", name: "yellow", price: 90},
+    {id: "cheese3", name: "white", price: 500},
+    {id: "cheese4", name: "green", price: 0},
+];
+
 const cheesePrint = () => {
-    let domString = '<div class="col-4">';
-    domString += '<h2>Cheese</h2>';
-    domString += '</div>'
+    let domString = '';
+    for (let i = 0; i < cheeses.length; i++)
+    domString += `
+    <div class="form-group form-check">
+    <input type="checkbox" class="form-check-input" id="${cheeses[i].id}">
+    <label class="form-check-label" for="${cheeses[i].id}">${cheeses[i].name}</label>
+    </div>
+    `
     utilities.printToDom('cheese-counter', domString);
 };
 
